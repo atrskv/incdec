ed2a0146a188fa9400de0020c29abce304a44068
lrayne <legaterayne@gmail.com>
2025-11-29 20:27:49 +0300

    test: add increment test
    # Conflicts:
    #	app/src/androidTest/java/com/example/inc_dec/IncrementButtonTest.kt

--- app/src/androidTest/java/com/example/inc_dec/IncrementButtonTest.kt
+++ app/src/androidTest/java/com/example/inc_dec/IncrementButtonTest.kt
@@ -3,28 +3,24 @@ package com.example.inc_dec
 import android.Manifest
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.rule.GrantPermissionRule
-import com.kaspersky.components.alluresupport.withAllureSupport
+import com.kaspersky.components.alluresupport.withForcedAllureSupport
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import org.junit.Rule
 import org.junit.Test
 
 class IncrementButtonTest : TestCase(
-    kaspressoBuilder = Kaspresso.Builder.withAllureSupport()
-
+    kaspressoBuilder = Kaspresso.Builder.withForcedAllureSupport()
 ) {
 
     @get:Rule
     val activityRule = activityScenarioRule<MainActivity>()
 
-
     @get:Rule
-
     val runtimePermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
         Manifest.permission.WRITE_EXTERNAL_STORAGE,
         Manifest.permission.READ_EXTERNAL_STORAGE
     )
-
     @Test
     fun incrementButtonShouldIncreaseValue() = run {
         step("Launch the app") {
@@ -34,10 +30,7 @@ class IncrementButtonTest : TestCase(
                     repeat(5) {
                         click()
                     }
-                }
-                valueText.hasText("Count: 5")
+                }                valueText.hasText("Count: 5")
             }
-        }
-    }
-
+        }    }
 }
