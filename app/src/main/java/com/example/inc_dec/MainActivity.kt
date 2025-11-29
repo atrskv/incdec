c69a1010cebc8c06cc557d3d0da7d0e21750835d
lrayne <legaterayne@gmail.com>
2025-11-29 20:05:19 +0300

    feat: implement actions

--- app/src/main/java/com/example/inc_dec/MainActivity.kt
+++ app/src/main/java/com/example/inc_dec/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.inc_dec
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var currentValue = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,25 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        findViewById<Button>(R.id.increment).setOnClickListener(this::incrementAction)
+        findViewById<Button>(R.id.decrement).setOnClickListener(this::decrementAction)
+        findViewById<Button>(R.id.reset).setOnClickListener(this::resetAction)
+    }
+
+    private fun incrementAction(view: View) {
+        updateCounterText(++currentValue)
+    }
+
+    private fun decrementAction(view: View) {
+        updateCounterText(--currentValue)
+    }
+
+    private fun resetAction(view: View) {
+        currentValue = 0
+        updateCounterText(currentValue)
+    }
+
+    private fun updateCounterText(value: Int) {
+        findViewById<TextView>(R.id.counter).text = String.format("Count: %d", value)
     }
 }
