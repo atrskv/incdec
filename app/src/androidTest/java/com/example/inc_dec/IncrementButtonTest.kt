bd8b23d07426a75203c6d6c66f57c07fac8c97f5
lrayne <legaterayne@gmail.com>
2025-11-29 20:27:49 +0300

    test: add increment test

---
+++ app/src/androidTest/java/com/example/inc_dec/IncrementButtonTest.kt
@@ -0,0 +1,43 @@
+package com.example.inc_dec
+
+import android.Manifest
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.rule.GrantPermissionRule
+import com.kaspersky.components.alluresupport.withAllureSupport
+import com.kaspersky.kaspresso.kaspresso.Kaspresso
+import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import org.junit.Rule
+import org.junit.Test
+
+class IncrementButtonTest : TestCase(
+    kaspressoBuilder = Kaspresso.Builder.withAllureSupport()
+
+) {
+
+    @get:Rule
+    val activityRule = activityScenarioRule<MainActivity>()
+
+
+    @get:Rule
+
+    val runtimePermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.READ_EXTERNAL_STORAGE
+    )
+
+    @Test
+    fun incrementButtonShouldIncreaseValue() = run {
+        step("Launch the app") {
+            MainScreen {
+                incrementButton.isDisplayed()
+                incrementButton {
+                    repeat(5) {
+                        click()
+                    }
+                }
+                valueText.hasText("Count: 5")
+            }
+        }
+    }
+
+}
