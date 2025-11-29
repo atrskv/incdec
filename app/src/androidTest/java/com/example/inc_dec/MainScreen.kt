32bba5fbe61ffcfc8b3503576934e9fe7fe49d0e
lrayne <legaterayne@gmail.com>
2025-11-29 20:10:48 +0300

    test: add screen

---
+++ app/src/androidTest/java/com/example/inc_dec/MainScreen.kt
@@ -0,0 +1,16 @@
+package com.example.inc_dec
+import com.kaspersky.kaspresso.screens.KScreen
+import io.github.kakaocup.kakao.text.KButton
+import io.github.kakaocup.kakao.text.KTextView
+
+object MainScreen : KScreen<MainScreen>() {
+
+    override val layoutId: Int = R.layout.activity_main
+    override val viewClass: Class<*> = MainActivity::class.java
+
+    val incrementButton = KButton { withId(R.id.increment) }
+    val decrementButton = KButton { withId(R.id.decrement) }
+    val resetButton = KButton { withId(R.id.reset) }
+
+    val valueText = KTextView {withId(R.id.counter)}
+}
